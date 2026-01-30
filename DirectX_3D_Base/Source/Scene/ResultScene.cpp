47ecb55cb9c3cdbaf05f4b1582c8ba282ff3313b
かいと <kuidouzhitian084@gmail.com>
2026-01-30 18:32:13 +0900

    ボタンのSE追加 パトカーのサイレンシーン切り替えで強制停止

--- DirectX_3D_Base/Source/Scene/ResultScene.cpp
+++ DirectX_3D_Base/Source/Scene/ResultScene.cpp
@@ -645,6 +645,12 @@ void ResultScene::Init()
             "BGM_GAMEOVER",
             0.8f
         );
+        
+        EntityFactory::CreateOneShotSoundEntity(
+            m_coordinator.get(),
+            "SE_ARREST",
+            0.6f  // リザルト画面なので少し大きめにしても良いかもしれません
+        );
     }
 
     // 下部ボタン
@@ -664,6 +670,17 @@ void ResultScene::Init()
 
 void ResultScene::Uninit()
 {
+
+    for (auto const& entity : m_coordinator->GetActiveEntities()) {
+        if (m_coordinator->HasComponent<SoundComponent>(entity)) {
+            auto& sound = m_coordinator->GetComponent<SoundComponent>(entity);
+            // assetIDでフィルタリングも可能
+            if (sound.assetID == "SE_ARREST") {
+                sound.RequestStop();
+            }
+        }
+    }
+
     // BGMエンティティの停止リクエスト
     if (m_bgmEntity != ECS::INVALID_ENTITY_ID)
     {
@@ -845,11 +862,12 @@ void ResultScene::CreateButtons()
         { 220.0f, 60.0f },                              // 【文字サイズ】
         { 20.0f, 5.0f },                                 // 【文字のズレ】
         []() {
+            
             ResultScene::isClear = false;
             SceneManager::ChangeScene<TitleScene>();
         }
     );
-
+    
 }
 
 void ResultScene::CreateNumberDisplay(int number, DirectX::XMFLOAT2 pos)
@@ -930,3 +948,5 @@ void OnStageClear(int clearedStageNo)
     // StageSelectSceneに戻ると、自動的に次のステージのページに切り替わり、
     // カードが出現演出される
 }
+
+
